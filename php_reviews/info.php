57da8c9e317c3e49cb31c3153f126647b9d7ff47
shalamnik <shalamberidze00@bk.ru>
2020-04-06 22:06:48 +0300

    add .gitignore

--- php_reviews/info.php
+++ php_reviews/info.php
@@ -1,25 +1,31 @@
-<?php
+<?php 
 
-function longestConsec($strarr, $k)
-{
+  if (isset($_POST['submit'])) {
 
-  $n = count($strarr);
+    session_start();
 
-  if ($n == 0 || $k > $n || $k <= 0) return "";
-  $arr = array_unique($strarr);
+    $_SESSION['name'] = $_POST['name'];
 
-  $sort_arr = [];
-  foreach ($arr as $elem) {
-    $sort_arr[strlen($elem)] = $elem;
+    setcookie('gender', $_POST['gender'], time() + 86400);
+
+    header('location: show.php');
   }
 
-  krsort($sort_arr);
-  
-  $max_str = implode(array_slice($sort_arr, 0, 2));
-
-  print_r($max_str); 
-}
-
-longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2);
-
 ?>
+
+<html>
+  <head>
+    <title>Test</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form action="#" method="POST">
+      <input type="text" name="name">
+      <select name="gender" id="select">
+        <option value="male">male</option>
+        <option value="female">female</option>
+      </select>
+      <input type="submit" name="submit" value="submit">
+    </form>
+  </body>
+</html>
