4fdfbd1ea48164d06f0c196a20fcce4c02095e81
shalamnik <shalamberidze00@bk.ru>
2020-04-14 19:03:45 +0300

    add database class

---
+++ php_reviews/modules/db.php
@@ -0,0 +1,40 @@
+<?php 
+
+class db
+{
+    protected $connection;
+    protected $query;
+    protected $show_errors = true;
+    protected $query_closed = true;
+    public $query_count = 0;
+
+    public function __construct($dbhost = 'localhost', $dbuser = 'root',
+                                $dbpass = '', $dbname = '', $charset = 'utf8')
+    {
+        $this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+        if ($this->connection->connect_error) {
+            $this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
+        }
+        $this->connection->set_charset($charset);
+    }
+
+    public function query($query)
+    {
+        if (!$this->query_closed) {
+            $this->query->close();
+        }
+        if ($this->query = $this->connection->prepare($query)) {
+            if (func_num_args() > 1) {
+                $x = func_get_args();
+                $args = array_slice($x, 1);
+                $types = '';
+                $args_ref = array();
+                foreach ($args as $k => &$arg) {
+                    
+                }
+            }
+        }
+    }
+}
+
+?>
